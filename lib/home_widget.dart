227617dd79633059abd0a5c6fd6475b67712c43b
Anna Munslow <annamunslow@gmail.com>
2020-02-13 19:15:58 -0500

    Initial commit

---
+++ lib/home_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'placeholder_widget.dart';
+
+class Home extends StatefulWidget {
+ @override
+ State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [ // list of our final widgets (pages)
+   PlaceholderWidget(Colors.deepOrange),
+   PlaceholderWidget(Colors.white),
+   PlaceholderWidget(Colors.green)
+ ];
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: Text('Air Emory'),
+     ),
+    body: _children[_currentIndex],
+     bottomNavigationBar: BottomNavigationBar(
+       onTap: onTabTapped, 
+       currentIndex: _currentIndex,
+       items: [
+         BottomNavigationBarItem(
+           icon: new Icon(Icons.library_books),
+           title: new Text('Report'),
+         ),
+         BottomNavigationBarItem(
+           icon: new Icon(Icons.map),
+           title: new Text('Map'),
+         ),
+         BottomNavigationBarItem(
+           icon: Icon(Icons.question_answer),
+           title: Text('FAQ')
+         )
+       ],
+     ),
+   );
+ }
+
+ void onTabTapped(int index) {
+   setState(() {
+     _currentIndex = index;
+   });
+ }
+}
