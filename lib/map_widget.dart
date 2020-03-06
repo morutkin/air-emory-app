f91bf59eb8b530320dae8af2d228d5cd5d309eba
morutkin <morutkin@gmail.com>
2020-03-06 12:31:55 -0500

    added google maps

--- lib/map_widget.dart
+++ lib/map_widget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
-import 'package:latlong/latlong.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
 
@@ -14,132 +12,27 @@ class MapWidget extends StatefulWidget {
 
 class _MapState extends State<MapWidget> {
 
+  GoogleMapController mapController;
 
-   List<Marker> markers;
-  int pointIndex;
-  List points = [
-    LatLng(32, -83),
-    LatLng(34, -80),
-  ];
+  final LatLng _center = const LatLng(45.521563, -122.677433);
 
-  @override
-  void initState() {
-    pointIndex = 0;
-    markers = [
-      Marker(
-        anchorPos: AnchorPos.align(AnchorAlign.center),
-        height: 30,
-        width: 30,
-        point: points[pointIndex],
-        builder: (ctx) => Icon(Icons.pin_drop),
-      ),
-      Marker(
-        anchorPos: AnchorPos.align(AnchorAlign.center),
-        height: 30,
-        width: 30,
-        point: LatLng(33.78857, -84.32232),
-        builder: (ctx) => Icon(Icons.pin_drop),
-      ),
-      Marker(
-        anchorPos: AnchorPos.align(AnchorAlign.center),
-        height: 30,
-        width: 30,
-        point: LatLng(33.78993, -84.32002),
-        builder: (ctx) => Icon(Icons.pin_drop),
-      ),
-      Marker(
-        anchorPos: AnchorPos.align(AnchorAlign.center),
-        height: 30,
-        width: 30,
-        point: LatLng(33.79741, -84.32342),
-        builder: (ctx) => Icon(Icons.pin_drop),
-      ),
-      Marker(
-        anchorPos: AnchorPos.align(AnchorAlign.center),
-        height: 30,
-        width: 30,
-        point: LatLng(33.79247, -84.32381),
-        builder: (ctx) => Icon(Icons.pin_drop),
-      ),
-      Marker(
-        anchorPos: AnchorPos.align(AnchorAlign.center),
-        height: 30,
-        width: 30,
-        point: LatLng(33.79334, -84.32151),
-        builder: (ctx) => Icon(Icons.pin_drop),
-      ),
-    ];
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+}
 
-    super.initState();
-  }
-
-  @override
+@override
   Widget build(BuildContext context) {
-    
-
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.refresh),
-        onPressed: () {
-          pointIndex++;
-          if (pointIndex >= points.length) {
-            pointIndex = 0;
-          }
-          setState(() {
-            markers[0] = Marker(
-              point: points[pointIndex],
-              anchorPos: AnchorPos.align(AnchorAlign.center),
-              height: 30,
-              width: 30,
-              builder: (ctx) => Icon(Icons.pin_drop),
-            );
-
-            // one of this
-            markers = List.from(markers);
-            // markers = [...markers];
-            // markers = []..addAll(markers);
-          });
-        },
-      ),
-      body: FlutterMap(
-        options: MapOptions(
-          center: points[0], // changing where the map centers "new LatLing (40, 40)"
-          zoom: 5,
-          plugins: [
-            MarkerClusterPlugin(),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
         ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: ['a', 'b', 'c'],
-          ),
-          MarkerClusterLayerOptions(
-            maxClusterRadius: 120,
-            size: Size(40, 40),
-            anchor: AnchorPos.align(AnchorAlign.center),
-            fitBoundsOptions: FitBoundsOptions(
-              padding: EdgeInsets.all(50),
-            ),
-            markers: markers,
-            polygonOptions: PolygonOptions(
-                borderColor: Colors.blueAccent,
-                color: Colors.black12,
-                borderStrokeWidth: 3),
-            builder: (context, markers) {
-              return FloatingActionButton(
-                child: Text(markers.length.toString()),
-                onPressed: null,
-              );
-            },
-          ),
-        ],
       ),
     );
-
-
   }
-
-
-  
 }
